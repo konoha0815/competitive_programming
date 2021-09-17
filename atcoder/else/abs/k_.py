5d7f56ecdcde9a604d8104db88d16d4671c2640e
konoha0815 <yamaar0815@gmail.com>
2021-09-18 02:13:29 +0900

    notitle

--- atcoder/else/abs/k_.py
+++ atcoder/else/abs/k_.py
@@ -9,12 +9,38 @@ t.insert(0, 0)
 x.insert(0, 0)
 y.insert(0, 0)
 
-print(t, x, y)
-for j in range(N - 1):
-    if (t[j+1] - t[j]) - (x[j+1] - x[j]) - (y[j+1] - y[j]) >= 0 and (t[j+1] - t[j]) - (x[j+1] - x[j]) - (y[j+1] - y[j])%2 == 0:
-        True
+for j in range(N):
+    T = t[j+1] - t[j]
+    X = abs(x[j+1] - x[j])
+    Y = abs(y[j+1] - y[j])
+    if  T - X - Y >= 0 and (T - X - Y)%2 == 0:
+        pass
     else:
         print("No")
         exit()
 
 print("Yes")
+
+
+
+
+# N = int(input())
+# t = [0] * N
+# x = [0] * N
+# y = [0] * N
+# for i in range(N):
+#     t[i], x[i], y[i] = map(int, input().split())
+
+# t.insert(0, 0)
+# x.insert(0, 0)
+# y.insert(0, 0)
+
+# print(t, x, y)
+# for j in range(N):
+#     if (t[j+1] - t[j]) - (x[j+1] - x[j]) - (y[j+1] - y[j]) >= 0 and (t[j+1] - t[j]) - (x[j+1] - x[j]) - (y[j+1] - y[j])%2 == 0:
+#         True
+#     else:
+#         print("No")
+#         exit()
+
+# print("Yes")
